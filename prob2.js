612851e75735831cf6b77c60f6ae8377da29fede
Shawaz <65177277+shawazi@users.noreply.github.com>
2022-12-13 12:41:25 -0500

    added third condition
    during review of this code, noticed that there was a 6th condition missing (num 1 and num 3 tied for greatest value).

--- prob2.js
+++ prob2.js
@@ -14,6 +14,8 @@ if (num1 > num2 && num1 > num3) {
     console.log(`${num1} and ${num2} are tied for the greatest value.`)
 } else if (num2 === num3 && num3 > num1) {
     console.log(`${num2} and ${num3} are tied for the greatest value.`)
+} else if (num1 === num3 && num1 > num2) {
+    console.log(`${num1} and ${num3} are tied for the greatest value.`)
 } else {
     console.log("All three numbers are equal to each other.")
 }
