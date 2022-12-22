f5e461738de7ee19f44b873e2099a98761ea4e81
shawazi <shawazisonfire@gmail.com>
2022-12-22 02:42:38 -0500

    assignments

--- JS_assignments_12_21/prob1.js
+++ JS_assignments_12_21/prob1.js
@@ -1,3 +1,21 @@
-function matrix(n) {
+function identityMatrix(n) {
     // code here that creates and then returns an n row by n column identity matrix.
+
+    // Thanks, GPT.
+
+    let matrix = "";
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            if (i === j) {
+                matrix += " 1";
+            } else {
+                matrix += " 0";
+            }
+        }
+        matrix += "\n";
+    }
+    return matrix;
 }
+
+let matrix = identityMatrix(50);
+console.log(matrix)
