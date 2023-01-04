a6336ad17582c97bedb5f91ad458c6f03acbc35c
shawazi <shawazisonfire@gmail.com>
2023-01-03 22:46:18 -0500

    problem 4 done, but it only finds a single pair, even if there are multiple possible pairs

--- JS_hw_1_3/prob4.js
+++ JS_hw_1_3/prob4.js
@@ -13,3 +13,6 @@ const list = [10, 1, 16, 15, 3, 7, 1, 16];
 const num = 17;
 
 console.log(anyTwo(list, num));
+
+
+// This works as a binary (yes or no) function, but it is unable to determine how many pairs from the input list are equal to the target. If there are multiple pairs that add up to the target, this function will only reveal the first pair it finds. 
