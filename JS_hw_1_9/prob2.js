7b9ce91857fbe9350d0ad5189b4f3a604613405f
shawazi <shawazisonfire@gmail.com>
2023-01-09 09:39:02 -0500

    prob 2 done, instead of using another loop to filter the duplicates from the result array, i used a set to get rid of the duplicates.

---
+++ JS_hw_1_9/prob2.js
@@ -0,0 +1,17 @@
+const list = [1, 2, -2, 4, 5, 4, 7, 8, 7, 7, 71, 3, 6];
+
+const duplicates = testarray => {
+    let result = new Set();
+    for (let i = 0; i < testarray.length; i++) {
+        for (let j = 0; j < testarray.length; j++) {
+            if (i !== j) {
+                if (testarray[i] === testarray[j]) {
+                    result.add(testarray[i]);
+                }
+            }
+        }
+    }
+    return result;
+}
+
+console.log(duplicates(list));
