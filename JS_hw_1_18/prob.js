a5dbf9a478040aa201bb18a36ea87670ba7f119e
shawazi <shawazisonfire@gmail.com>
2023-01-18 15:06:12 -0500

    prob1 done

---
+++ JS_hw_1_18/prob.js
@@ -0,0 +1,23 @@
+const colors = ["yellow", "blue", "green", "red"]
+
+// let randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+// randomColor = '#' + ('000000' + randomColor).slice(-6);
+
+const div = document.getElementById("Prob1");
+const button = document.getElementById("colorChange");
+let colorIndex = 0;
+
+button.addEventListener("click", function() {
+    
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+
+    randomColor = '#' + ('000000' + randomColor).slice(-6);
+
+    div.style.backgroundColor = randomColor;
+    
+    // colors[colorIndex];
+    // colorIndex = (colorIndex + 1) % colors.length;
+    
+    // the above commented out code is using the array with 4 options, but this current version of the code should be using all possible CSS colors as the options for the random background color.
+});
--- js_hw_1_11/prob1.js
+++ js_hw_1_11/prob1.js
@@ -1,22 +1,22 @@
-// function count(string) {
-//     let string2 = string.replace(/\s/g, '');
-//     const vowels = ["a", "e", "i", "o", "u"];
-//     const cons = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+function count(string) {
+    let string2 = string.replace(/\s/g, '');
+    const vowels = ["a", "e", "i", "o", "u"];
+    const cons = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
 
-//     let strArr = string2.toLowerCase().split("");
-//     let vowelCount = 0;
-//     let consCount = 0;
-//     for (let i = 0; i < strArr.length; i++) {
-//         if (vowels.includes(strArr[i])) {
-//             vowelCount++;
-//         }
-//         else if (cons.includes(strArr[i])) {
-//             consCount++;
-//         }
-//     }
-//     return `There are ${consCount} consonants and ${vowelCount} vowels in "${string}".`
+    let strArr = string2.toLowerCase().split("");
+    let vowelCount = 0;
+    let consCount = 0;
+    for (let i = 0; i < strArr.length; i++) {
+        if (vowels.includes(strArr[i])) {
+            vowelCount++;
+        }
+        else if (cons.includes(strArr[i])) {
+            consCount++;
+        }
+    }
+    return `There are ${consCount} consonants and ${vowelCount} vowels in "${string}".`
 
-// }
+}
 
 
 // const vowels = ["a", "e", "i", "o", "u"];
@@ -29,6 +29,6 @@
 //     } else if (cons.includes(element.toUpperCase()))))
 // })
 
-// const test = "Write a JS code that counts how many vowels and constants a string has that a user entered";
+const test = "Write a JS code that counts how many vowels and constants a string has that a user entered";
 
-// console.log(count(test))
+console.log(count(test))
